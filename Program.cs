5e20b4da9a3860b568e4741faa5568bd41c0400d
Natalya <07_nata@mail.ru>
2023-02-14 15:39:34 +0300

    решена задача

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+string[] array = inputArray();
+string[] inputArray()
+{
+    Write("Введите значения через пробел: ");
+    return ReadLine().Split(" ");
+}
+string[] result = FindLessThan(array, 3);
+WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+
+string[] FindLessThan(string[] input, int n)
+{
+    string[] output = new string[CountLessThan(input, n)];
+    for (int i = 0, j = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n)
+        {
+            output[j] = input[i];
+            j++;
+        }
+    }
+    return output;
+}
+
+int CountLessThan(string[] input, int n)
+{
+    int count = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n)
+        {
+            count++;
+        }
+    }
+    return count;
+}
